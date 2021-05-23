93b53d27f51dcaacb2c3d4727ab8e1f620508e4b
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-24 04:42:44 +0530

    refactoring of code and using switchcase

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,18 +4,20 @@ echo "Welcome to Employee Wage Computation Program"
 isPartTime=2
 isFullTime=1
 empRatePerHr=20
-randomCheck=$((RANDOM%3))
+empCheck=$((RANDOM%3))
 
-if [ $isFullTime -eq $randomCheck ]
-then
-	echo "Employee is full-time present"
-	empHrs=8
-elif [ $isPartTime -eq $randomCheck ]
-then
-	echo "Employee is part-time present"
-	empHrs=8
-else
-	echo "Employee is absent"
-	empHrs=0
-fi
+case $empCheck in
+	 $isFullTime) 
+			echo "Employee is full-time present"
+			empHrs=8
+				;;
+	$isPartTime)
+			echo "Employee is part-time present"
+			empHrs=8
+				;;
+		*) 
+			echo "Employee is absent"
+			empHrs=0
+				;;
+esac
 salary=$(($empHrs*$empRatePerHr))
